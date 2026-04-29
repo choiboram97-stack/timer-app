cd3fd98549a04d0de05ec4e965fe5596ba8e7dbe
choiboram97-stack <choiboram97@gmail.com>
2026-04-29 18:07:54 +0900

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "접수 타이머",
+  description: "고객 접수 시간 관리",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
